8c58a58c32c7dfaae801eaa80fa19979c30ba9df
Tara Tandel <tara.tandel@icloud.com>
2020-04-01 00:37:27 +0200

    Unzip The assignment

---
+++ A03 - Advanced transforms/move.js
@@ -0,0 +1,27 @@
+function move() {
+	// Rotate 45 degrees around an arbitrary axis passing through (1,0,-1). The x-axis can be aligned to the arbitrary axis after a rotation of 30 degrees around the z-axis, and then -60 degrees around the y-axis.
+	var R1 =[1.0,		0.0,		0.0,		0.0,
+			   0.0,		1.0,		0.0,		0.0,
+			   0.0,		0.0,		1.0,		0.0,
+			   0.0,		0.0,		0.0,		1.0];					   
+	// Half the size of an object, using as fixed point (5,0,-2)
+	var S1 = [1.0,		0.0,		0.0,		0.0,
+			   0.0,		1.0,		0.0,		0.0,
+			   0.0,		0.0,		1.0,		0.0,
+			   0.0,		0.0,		0.0,		1.0];
+			   
+	// Mirror the starship along a plane passing through (1,1,1), and obtained rotating 15 degree around the x axis the xz plane
+	var S2 =  [1.0,		0.0,		0.0,		0.0,
+			   0.0,		1.0,		0.0,		0.0,
+			   0.0,		0.0,		1.0,		0.0,
+			   0.0,		0.0,		0.0,		1.0];
+			   
+	// Apply the inverse of the following sequence of transforms: Translation of (0, 0, 5) then rotation of 30 degree around the Y axis, and finally a uniform scaling of a factor of 3.
+	var I1 =  [1.0,		0.0,		0.0,		0.0,
+			   0.0,		1.0,		0.0,		0.0,
+			   0.0,		0.0,		1.0,		0.0,
+			   0.0,		0.0,		0.0,		1.0];
+
+	return [R1, S1, S2, I1];
+}
+
