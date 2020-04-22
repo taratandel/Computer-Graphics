dc0c25efbfca8bd4d13e0f555b35a12228296b6f
Tara Tandel <tara.tandel@icloud.com>
2020-04-22 18:42:40 +0200

    finish hexagon

--- A08 - List strip and fan/models.js
+++ A08 - List strip and fan/models.js
@@ -16,13 +16,20 @@ function buildGeometry() {
 
 	addMesh(vert2, "S", [0.0, 0.0, 1.0]);
 
-
+	let coordinates = calculatingHexagonVertixCoordinates(0,0,1)
 	// Draws a flat hexagon (replace the vertices)
-	var vert3 = [[-1.0,-1.0, 1.0], [1.0,-1.0, 1.0], [1.0,1.0, 1.0], [-1.0,1.0, 1.0], [0.0,1.5, 1.0],
-				 [-1.0,-1.0,-1.0], [1.0,-1.0,-1.0], [1.0,1.0,-1.0], [-1.0,1.0,-1.0], [0.0,1.5,-1.0]
+	var vert3 = [[0.0,0.0, 0.0], coordinates[0], coordinates[1], coordinates[2], coordinates[3],
+		coordinates[4], coordinates[5], coordinates[6]
 	];
 
 	addMesh(vert3, "F", [0.0, 1.0, 0.0]);
 	
 }
 
+function calculatingHexagonVertixCoordinates(centerX, centerY, size) {
+	var coordinates = []
+	for (i = 0; i < 7; i++) {
+		coordinates.push([(size*Math.cos(utils.degToRad(0+i*60)))+centerX,(size*Math.sin(utils.degToRad(0+i*60)))+centerY, 0.0])
+	}
+	return coordinates
+}
