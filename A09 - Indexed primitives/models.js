b68327b5834404389f5722455acdc7045be3baa7
skings <samanf74@gmail.com>
2020-04-24 17:01:21 +0200

    make a simple donot format

--- A09 - Indexed primitives/models.js
+++ A09 - Indexed primitives/models.js
@@ -93,8 +93,56 @@ function buildGeometry() {
 	
 	
 	// Draws a Torus -- To do for the assignment
-	var vert4 = [[-1.0,-1.0,0.0], [1.0,-1.0,0.0], [-1.0,1.0,0.0], [1.0,1.0,0.0]];
-	var ind4 = [0, 1, 2,   1, 3, 2];
+	var vert4 = [];
+	///// Creates vertices
+	k = 0;
+	// for(j = 0; j < 1; j++) {
+	j = 0;
+	j_last = 36;
+	j_start = 0;
+	for (j = j_start; j < j_last; j++) {
+		vert4[k++] = [ - 2.0 * Math.cos(j * 10.0 / 180.0 * Math.PI), 0.0, - 2.0 * Math.sin(j * 10.0 / 180.0 * Math.PI)];
+		for (i = 0; i < 36; i++) {
+			x = (Math.sin(i * 10.0 / 180.0 * Math.PI)  - 2.0) * Math.cos(j * 10.0 / 180.0 * Math.PI);
+			y = (Math.cos(i * 10.0 / 180.0 * Math.PI)) / 2.0;
+			z = (Math.sin(i * 10.0 / 180.0 * Math.PI)  - 2.0) * Math.sin(j * 10.0 / 180.0 * Math.PI);
+			vert4[k++] = [x, y, z];
+		}
+	}
+	console.log(vert4.length)
+	// }
+
+	// for(j = 0; j < 648; j++) {
+	// 	x = 2 * /*Math.sin(i*10.0/180.0*Math.PI) **/ Math.sin(j*10.0/180.0*Math.PI);
+	// 	y = 0.0;/*2 * Math.cos(j*10.0/180.0*Math.PI);*/
+	// 	z = 2 * Math.cos(j*10.0/180.0*Math.PI);/*/!*Math.cos(i*10.0/180.0*Math.PI) **!/ Math.sin(j*10.0/180.0*Math.PI);*/
+	// 	vert4[k++] = [x, y, z];
+	// }
+	// }
+	// vert4[k++] = [0.0,-1.0,0.0];
+
+	////// Creates indices
+	var ind4 = [];
+	k = 0;
+	for (j = j_start; j < j_last; j++) {
+		for (i = 1; i < 37; i++) {
+			ind4[k++] = j * 37;
+			ind4[k++] = j * 37 + i;
+			ind4[k++] = j * 37 + i + 1;
+
+		}
+		ind4[ind4.length - 1] = j * 37 + 1;
+	}
+
+	// for(i=37; i < 37 + 36; i++) {
+	// 	ind4[k++] = 0;
+	// 	ind4[k++] = i;
+	// 	ind4[k++] = i + 1;
+	// }
+	// ind4[ind4.length - 1] = 37;
+
+	console.log(ind4)
+
 	var color4 = [1.0, 1.0, 0.0];
 	addMesh(vert4, ind4, color4);
 }
