9ebdfe6206f2e5d9485583657206c1039e9e8bf7
saman <samanf74@gmail.com>
2020-07-22 11:46:12 +0430

    auto merge was damaged the assignment :? fuck this kind of merge

--- A10b - GLSL BRDF/shaders.js
+++ A10b - GLSL BRDF/shaders.js
@@ -1,7 +1,5 @@
 function shaders() {
 // The shader can find the required informations in the following variables:
-
-<<<<<<< HEAD
 //vec3 fs_pos;		// Position of the point in 3D space
 //
 //float SpecShine;		// specular coefficient for both Blinn and Phong
@@ -26,41 +24,11 @@ function shaders() {
 //
 //vec4 lightColorA;
 //vec4 lightColorB;
-=======
-//vec3 fs_pos;		// Position of the point in 3D space
-//
-//float SpecShine;		// specular coefficient for both Blinn and Phong
-//float DToonTh;		// Threshold for diffuse in a toon shader
-//float SToonTh;		// Threshold for specular in a toon shader
-//
-//vec4 diffColor;		// diffuse color
-//vec4 ambColor;		// material ambient color
-//vec4 specularColor;		// specular color
-//vec4 emit;			// emitted color
-//	
-//vec3 normalVec;		// direction of the normal vecotr to the surface
-//vec3 eyedirVec;		// looking direction
-//
-//
-// Lighr directions can be found into:
-//vec3 lightDirA;
-//vec3 lightDirB;
-//vec3 lightDirC;
-//
-//and intensity is returned into:
-//
-//vec4 lightColorA;
-//vec4 lightColorB;
->>>>>>> master
 //vec4 lightColorC;
 //
 // Ambient light contribution can be found intop
 //
-<<<<<<< HEAD
 // vec4 ambientLight;
-=======
-// vec4 ambientLight;
->>>>>>> master
 
 // Lambert diffuse and Ambient material
 var S1 = `
@@ -86,22 +54,15 @@ var S2 = `
 
 // No diffuse, ambient and Blinn specular
 var S3 = `
-<<<<<<< HEAD
-	out_color = vec4(1.0, 0.0, 1.0, 1.0);
-=======
 	vec4 sLAcontr = pow(clamp(dot(normalize(eyedirVec+lightDirA), normalVec),0.0,1.0), SpecShine) * lightColorA;
 	vec4 sLBcontr = pow(clamp(dot(normalize(eyedirVec+lightDirB), normalVec),0.0,1.0), SpecShine) * lightColorB;
 	vec4 sLCcontr = pow(clamp(dot(normalize(eyedirVec+lightDirC), normalVec),0.0,1.0), SpecShine) * lightColorC;
 	vec4 specular = specularColor * (sLAcontr + sLBcontr + sLCcontr);
 	out_color = clamp(specular +  ambientLight * ambColor, 0.0, 1.0);
->>>>>>> master
 `;
 
 // Diffuse and Phong specular
 var S4 = `
-<<<<<<< HEAD
-	out_color = vec4(0.0, 1.0, 0.0, 1.0);
-=======
 	vec3 rA = -reflect(lightDirA,normalVec);
 	vec3 rB = -reflect(lightDirB,normalVec);
 	vec3 rC = -reflect(lightDirC,normalVec);
@@ -117,14 +78,10 @@ var S4 = `
 	vec4 diffuse = diffColor * (dLAcontr + dLBcontr + dLCcontr);
 	
 	out_color = clamp(diffuse + specular, 0.0, 1.0);
->>>>>>> master
 `;
 
 // Diffuse, ambient, emission and Phong specular
 var S5 = `
-<<<<<<< HEAD
-	out_color = vec4(0.0, 0.0, 1.0, 1.0);
-=======
 	vec3 rA = -reflect(lightDirA,normalVec);
 	vec3 rB = -reflect(lightDirB,normalVec);
 	vec3 rC = -reflect(lightDirC,normalVec);
@@ -142,14 +99,11 @@ var S5 = `
 	vec4 ambient = ambientLight * ambColor;
 	
 	out_color = clamp(diffuse+ specular + ambient + emit, 0.0, 1.0);;
->>>>>>> master
 `;
 
 // Ambient + Oren-Nayar with roughness sigma=0.5 (consider only Light A)
 var S6 = `
-<<<<<<< HEAD
 	out_color = vec4(0.0, 1.0, 1.0, 1.0);
-=======
 	float p = max(0.0, dot(lightDirA, normalVec));
 	float q = max(0.0, dot(eyedirVec, normalVec));
 	
@@ -172,7 +126,6 @@ var S6 = `
 	vec4 ambient = ambientLight * ambColor;
 	
 	out_color = clamp(diffuse + ambient, 0.0, 1.0); 
->>>>>>> master
 `;
 
 	return [S1, S2, S3, S4, S5, S6];
