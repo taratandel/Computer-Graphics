7491fbe22ddd37275e2301188782af4daa187054
Tara Tandel <tara.tandel@icloud.com>
2020-07-24 16:36:14 +0200

    Comment A04a

--- A04a - Parallel projections/parallel.js
+++ A04a - Parallel projections/parallel.js
@@ -1,16 +1,18 @@
 function parallel() {
   // Build a parallel projection matrix, for a 16/9 viewport, with halfwidt w=40, near plane n=1, and far plane f=101.
-
+  //first it moves the center of the projection box in the origin with a translation
+  //  normalizes the coordinates between -1 and 1 by performing a scale transformation
+  //  The last step converts the system from right-handed to left-handed, by inverting the z-axis with the mirror transformation
   // formulas
   // l = -w
   // r = w
   // t = w / a
   // b = -w / a
 
-  w = 40.0
-  f = 101.0
-	n = 1.0
-  a = 16.0 / 9.0
+  let w = 40.0
+  let f = 101.0
+  let n = 1.0
+  let a = 16.0 / 9.0
 
 
   var out =
