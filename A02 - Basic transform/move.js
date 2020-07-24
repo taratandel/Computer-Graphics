5c2330013947d0c6a7c96d810f1df5618e6515e1
Tara Tandel <tara.tandel@icloud.com>
2020-07-24 12:32:53 +0200

    comment A02

--- A02 - Basic transform/move.js
+++ A02 - Basic transform/move.js
@@ -1,5 +1,9 @@
 function move() {
 	// Translate of +2 on the x axis, and -1 on the y axis
+	// |1 0 0 dx|
+	// |0 1 0 dy|
+	// |0 0 1 dz|
+	// |0 0 0 1 |
 	var T1 =
 		[
 				1.0,		0.0,		0.0,		2.0,
@@ -9,6 +13,24 @@ function move() {
 		];
 
 	// Rotate of 45 degrees on the x axis
+	// ################################## //
+	// Rx(α)=
+	// |1 0     0    0|
+	// |0 cosα −sinα 0|
+	// |0 sinα  cosα 0|
+	// |0 0     0    1|
+	// ################################## //
+	// Ry(α)=
+	// |cosα  0 sinα 0|
+	// |0     1  0   0|
+	// |−sinα 0 cosα 0|
+	// |0     0 0    1|
+	// ################################## //
+	// Rz(α)=
+	// |cosα −sinα 0 0|
+	// |sinα  cosα 0 0|
+	// |0     0    1 0|
+	// |0     0    0 1|
 	let alpha = toRadians(45)
 	var R1 =
 		[
@@ -19,6 +41,10 @@ function move() {
 		];
 
 	// Make the object 2 times smaller
+	// |sx 0  0  0|
+	// |0  sy 0  0|
+	// |0  0  sz 0|
+	// |0  0  0  1|
 	var S1 =
 		[
 			0.5,		0.0,		0.0,		0.0,
@@ -36,6 +62,7 @@ function move() {
 		];
 
 	// Mirror over the z axis
+	// It is obtained by assigning -1 to all the scaling factors but the one of the axis
 	var S3 =
 		[
 			-1.0,		0.0,		0.0,		0.0,
@@ -45,6 +72,7 @@ function move() {
 		];
 
 	// Flatten over the z axis
+	//remove the 1 on the desired axis
 	var S4 =
 		[
 			1.0,		0.0,		0.0,		0.0,
@@ -54,6 +82,24 @@ function move() {
 		];
 
 	// Make a shear along the Y axis, with a factor of 1 along the x axis
+	// ################################## //
+	// Hx(hy,hz)=
+	//|1  0 0 0|
+	//|hy 1 0 0|
+	//|hz 0 1 0|
+	//|0  0 0 1|
+	// ################################## //
+	//Hy(hx,hz)=
+	// |0 hz 1 0|
+	// |1 hx 0 0|
+	// |0 1  0 0|
+	// |0 0  0 1|
+	// ################################## //
+	//Hz(hx,hy)=
+	// |0 0  1 0|
+	// |1 0 hx 0|
+	// |0 1 hy 0|
+	// |0 0 0  1|
 	var H1 =
 		[
 			1.0,		1.0,		0.0,		0.0,
