bc6a9071cdbb94bf31fd02f211f032a47da34015
Tara Tandel <tara.tandel@icloud.com>
2020-07-24 17:11:03 +0200

    Comment A04b

--- A04b - Axonometry/axonometry.js
+++ A04b - Axonometry/axonometry.js
@@ -1,5 +1,8 @@
 function axonometry() {
 	// Make an isometric view, w = 50, a = 16/9, n = 1, f = 101.
+	//Axonometric projections, overcome this limitation, by showing all the faces of a cube at the same time.
+	//Axonometric projections where rays are perpendicular to the projection plane are called Orthographic projections.
+	//In isometric projections the three axes are angled at 120o one from the other.
 	let w = 50.0;
 	let a = 16.0 / 9.0;
 	let n = 1;
@@ -17,7 +20,10 @@ function axonometry() {
 	// parallel orthogonal x (x rotation) x (y rotation)
 	A1 = utils.multiplyMatrices(p, alpha);
 	A1 = utils.multiplyMatrices(A1, beta);
-			   
+	// Dimetric projections have two different units: one for the x and z-axis and one for the y-axis.
+	//The angle between the x and y-axis is equal to the one between the y and z-axis, but different
+	// from the one between the z and x-axis
+	//rotation of ±45o around the y-axis, followed by an arbitrary rotation a around the x-axis
 	// Make a dimetric view, w = 50, a = 16/9, n = 1, f = 101, rotated 20 around the x-axis
 	var A2 =  [0.05,	0.0,		0.0,		0.0,
 			   0.0,		0.05,		0.0,		0.0,
@@ -30,8 +36,8 @@ function axonometry() {
 	// parallel orthogonal x (x rotation) x (y rotation)
 	A2 = utils.multiplyMatrices(p, alpha);
 	A2 = utils.multiplyMatrices(A2, beta);
-
-			   
+	//Trimetric projections are obtained by applying an arbitrary rotation b around the y-axis, followed
+	// by an arbitrary rotation a around the x-axis,
 	// Make a trimetric view, w = 50, a = 16/9, n = 1, f = 101, rotated -30 around the x-axis and 30 around the y-axis
 	var A3 =  [0.05,	0.0,		0.0,		0.0,
 			   0.0,		0.05,		0.0,		0.0,
@@ -40,7 +46,9 @@ function axonometry() {
 
 	alpha = utils.MakeRotateXMatrix(-30); // trimetric project ask for rotate x-axis -30.0
 	beta = utils.MakeRotateYMatrix(30); // trimetric project ask for rotate y-axis 30.0
-
+	//In oblique projections rays are parallel, but oblique with respect to the projection plane.
+	//If the length is maintained, the projection is called Cavalier, otherwise it is called Cabinet.
+	// can be obtained by applying a shear along the z-axis before the orthogonal projection
 	// parallel orthogonal x (x rotation) x (y rotation)
 	A3 = utils.multiplyMatrices(p, alpha);
 	A3 = utils.multiplyMatrices(A3, beta);
